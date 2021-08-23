6ed2d713325deb5799e2c6a2dacdef3ad0958ca0
DamanChen <boychen@aliyun.com>
2021-08-23 15:10:28 +0800

    单文件组件及项目

---
+++ test-project/src/components/test.vue
@@ -0,0 +1,16 @@
+<template>
+	<h3 class="red"> Cfq test </h3>
+</template>
+
+<script>
+export default {
+	name: 'test',
+	props: {
+		msg: String
+	}
+}
+</script>
+
+<style>
+	.red{color: red;}
+</style>
